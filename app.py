779dc28175771155b9022ab92ddc8026cd3728f2
sfsf-mj <105579075+sfsf-mj@users.noreply.github.com>
2024-11-14 00:37:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 # تحميل النموذج
-model = tf.keras.models.load_model("plant_model.keras")
+model = tf.keras.models.load_model("plantodel.keras")
 
 # كود المعالجة السابقه مفروض يتعدل على حسب أخر نموذج (معلوووومه مهههههمه) لازم تتعدل
 def preprocess_image(image):
